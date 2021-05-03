31fcb465ef20e888ccea230a86243d0f006ecfbc
Aditya Anand <60457205+weirdrag08@users.noreply.github.com>
2021-05-03 23:39:34 +0530

    Adding source code for Introduction in C++

---
+++ C++/Introduction/InputOutput.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+   int num1 , num2 , num3;
+   cin >> num1 >> num2 >> num3;
+   cout << num1 + num2 + num3 << endl;
+    return 0;
+}
