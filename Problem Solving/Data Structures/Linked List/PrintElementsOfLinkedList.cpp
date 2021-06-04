30fa72c7f470033cf180470f1f2e25077f9de616
Aditya Anand <60457205+weirdrag08@users.noreply.github.com>
2021-06-04 21:47:36 +0530

    Adding source code for Print Elements Of LinkedList

---
+++ Problem Solving/Data Structures/Linked List/PrintElementsOfLinkedList.cpp
@@ -0,0 +1,22 @@
+
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+
+   if(head == NULL){
+       return;
+   }
+   cout << head-> data<< endl;
+   printLinkedList(head-> next);
+}
+
